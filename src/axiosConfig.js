83d5f8b31fcfc3ee6ff58761e68d218eb021aa50
Piyush Hadiya <hadiyapiyush07@gmail.com>
2026-08-18 21:08:45 +0530

    chore: Update backend API URL to live Render link

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api', // adjust to your backend URL
+  baseURL: 'https://cms-backend-wl7u.onrender.com/api', // adjust to your backend URL
 });
 
 axiosInstance.interceptors.request.use(
